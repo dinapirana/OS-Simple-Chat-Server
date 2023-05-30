6b8f2b0fe5addc9083a07a259d25215411e1ae45
diellzapp <122102145+diellzapp@users.noreply.github.com>
2023-05-30 16:50:50 +0200

    Update client.c

--- client.c
+++ client.c
@@ -67,3 +67,133 @@ void* receive_messages(void* socket_desc) {
     pthread_exit(NULL);
 
 }
+int main() {
+
+    int client_socket;
+
+    struct sockaddr_in server_address;
+
+    pthread_t receive_thread;
+
+
+
+    // Krijimi i client socket
+
+   
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (client_socket < 0) {
+
+        perror("Ka ndodhur nje gabim gjate krijimit te client socket");
+
+        exit(1);
+
+    }
+
+
+
+   
+
+    memset((char*)&server_address, 0, sizeof(server_address));
+
+    server_address.sin_family = AF_INET;
+
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    server_address.sin_port = htons(8888);
+
+
+
+    // Lidhu me serverin
+
+   
+
+    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+
+        perror("Ka ndodhur nje gabim gjate lidhjes me server");
+
+        exit(1);
+
+    }
+
+
+
+ 
+
+    if (pthread_create(&receive_thread, NULL, receive_messages, (void*)&client_socket) != 0) {
+
+        perror("Ka ndodhur nje gabim gjate marrjes se thread - ave ");
+
+        exit(1);
+
+    }
+
+
+
+   
+
+    char message[MAX_BUFFER_SIZE];
+
+    while (1) {
+
+        fgets(message, MAX_BUFFER_SIZE, stdin);
+
+
+
+       
+
+        size_t length = strlen(message);
+
+        if (length > 0 && message[length - 1] == '\n') {
+
+            message[length - 1] = '\0';
+
+        }
+
+
+
+      // Komanda '/quit' nese klienti do te shkycet
+
+     
+
+        if (strcmp(message, "/quit") == 0) {
+
+            if (send(client_socket, message, strlen(message), 0) < 0) {
+
+                perror("Ka ndodhur nje gabim gjate dergimit te komandes ne server");
+
+                exit(1);
+
+            }
+
+            break;
+
+        }
+
+
+
+       
+
+        if (send(client_socket, message, strlen(message), 0) < 0) {
+
+            perror("Ka ndodhur nje gabim gjate dergimit te mesazhit ne server");
+
+            exit(1);
+
+        }
+
+    }
+
+
+
+   
+
+    pthread_join(receive_thread, NULL);
+
+
+
+    return 0;
+
+}
+
